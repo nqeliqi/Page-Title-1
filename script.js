36f98b6464f1ae64ffe850b44e6cc0fc1c7d614f
nqeliqi <43378819+nqeliqi@users.noreply.github.com>
2019-02-08 09:48:05 -0500

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 $(".soccer").click(function(){
     $("img").show();
 });
+
+$("img").click(function(){
+    $(".text").show();
+});
+
+$(".text").click(function(){
+    $("background-color").show();
+});
